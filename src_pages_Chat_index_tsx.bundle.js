805f443cbfdd347fc4abdec087361c9cb5e89ace
Соковнин Максим Евгеньевич <maxim.sokovnin@gmail.com>
2021-11-01 18:25:14 +0400

    Updates

---
+++ src_pages_Chat_index_tsx.bundle.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkfrontend"] = self["webpackChunkfrontend"] || []).push([["src_pages_Chat_index_tsx"],{
+
+/***/ "./src/pages/Chat/index.tsx":
+/*!**********************************!*\
+  !*** ./src/pages/Chat/index.tsx ***!
+  \**********************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ \"./node_modules/react/jsx-runtime.js\");\n/* harmony import */ var _ui__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @ui */ \"./src/ui/index.ts\");\n/* harmony import */ var _components__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @components */ \"./src/components/index.ts\");\n\n\n\n\nvar Chat = function Chat() {\n  return (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_ui__WEBPACK_IMPORTED_MODULE_1__.Main, {\n    children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_components__WEBPACK_IMPORTED_MODULE_2__.Chat, {}, void 0)\n  }, void 0);\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Chat);\n\n//# sourceURL=webpack://frontend/./src/pages/Chat/index.tsx?");
+
+/***/ })
+
+}]);
