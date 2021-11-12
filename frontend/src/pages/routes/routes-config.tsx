0e78076ca81a3e3592ccbc828ec245bf0c1ef74c
Impeqq <sokovnin.m@list.ru>
2021-11-12 23:30:13 +0400

    remove lazy pages

--- frontend/src/pages/routes/routes-config.tsx
+++ frontend/src/pages/routes/routes-config.tsx
@@ -1,12 +1,10 @@
-import { lazy } from "react";
+import Auth from "@pages/Auth";
+import Chat from "@pages/Chat";
+import Error404 from "@pages/Error404";
+import Home from "@pages/Home";
+import Profile from "@pages/Profile";
 import { RouteProps } from "react-router-dom";
 import { routePath } from "./route-path";
-
-const Error404 = lazy(() => import("@pages/Error404"));
-const Home = lazy(() => import("@pages/Home"));
-const Chat = lazy(() => import("@pages/Chat"));
-const Auth = lazy(() => import("@pages/Auth"));
-const Profile = lazy(() => import("@pages/Profile"));
 
 export type TRoutesConfiguration = RouteProps & {
   routes?: RouteProps[];
