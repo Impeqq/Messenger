b59530527f87381f375cd77027ca0bfe9b6ed707
Impeqq <sokovnin.m@list.ru>
2021-11-20 02:33:02 +0400

    Updates

--- static/f5d4743f5936cbdeb3d2.js
+++ static/f5d4743f5936cbdeb3d2.js
@@ -3935,7 +3935,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	!function() {
-/******/ 		__webpack_require__.p = "Messenger/";
+/******/ 		__webpack_require__.p = "/Messenger";
 /******/ 	}();
 /******/ 	
 /************************************************************************/
